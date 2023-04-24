8120143a3efc510963dbf4b56fd19bd2bc1d13de
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-04-24 11:46:27 -0300

    users crud

---
+++ src/modules/users/index.ts
@@ -0,0 +1,66 @@
+import { FastifyInstance } from "fastify";
+
+import crypto from 'node:crypto';
+
+import { knex } from "../../database";
+import { generateToken } from "../../middleware";
+import { CreateUserProps } from "./interface";
+
+const JWT = process.env.JWT;
+
+export const User = {
+  login: async(app: FastifyInstance) => app.post('/login-user', async(request, reply) => {
+    const { cpf, password } = request.body as CreateUserProps;
+
+    return knex('users')
+      .where({ cpf, password })
+      .first()
+      .then(response =>
+        reply
+        .status(201)
+        .cookie('session_id', response.id, { path: '/', maxAge: 1000 * 60 * 60 * 24 * 7 })
+        .cookie('token', generateToken({ id: JWT }), { path: '/', maxAge: 1000 * 60 * 60 * 24 * 1 })
+        .send()
+      )
+      .catch(err =>
+        reply.status(400).send({
+          error: err
+        })
+      )
+  }),
+  create: async(app: FastifyInstance) => app.post('/register-user', async(request, reply) => {
+    const { name, email, password, repeat_password, cpf } = request.body as CreateUserProps;
+    // let sessionId = request.cookies.sessionId;
+
+    // if(!sessionId){
+    //   sessionId = crypto.randomUUID();
+
+    //   reply.cookie('sessionId', sessionId, {
+    //     path: '/',
+    //     maxAge: 1000 * 60 * 60 * 24 * 7
+    //     /**
+    //      * @name 1000_one_milliseconds;
+    //      * @name 60_one_minute;
+    //      * @name 60_one_hours;
+    //      * @name 24_one_days;
+    //      * @name 7_seven_days;
+    //      */
+    //   })
+    // }
+
+    // const example = {
+    //   token: null,
+    //   user: null,
+    // };
+
+    return knex('users').insert({
+      id: crypto.randomUUID(),
+      name,
+      email,
+      password,
+      cpf
+    })
+      .then((response) => reply.status(201).send({ message: response }))
+      .catch((err) => reply.status(400).send({ error: err }))
+  })
+}
---
+++ src/modules/users/interface.ts
@@ -0,0 +1,13 @@
+// import {  } from 'fastify';
+
+interface CreateUserProps {
+  cpf: number;
+  name: string;
+  email: string;
+  password: string;
+  repeat_password: string;
+};
+
+export {
+  CreateUserProps
+};
