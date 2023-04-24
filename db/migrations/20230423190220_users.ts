521e88d03bda5acbfebe33103a044b6d085d27ce
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-04-24 11:31:31 -0300

    migrations

---
+++ db/migrations/20230423190220_users.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('email').unique().notNullable()
+    table.text('password').notNullable()
+    table.decimal('cpf', 11, 2).unique().notNullable()
+    table.timestamp('create_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
+
