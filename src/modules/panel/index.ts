64b69ebccf445122d12bccd978ce2a0db23a7c17
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-04-24 12:01:18 -0300

    change get to post

--- src/modules/panel/index.ts
+++ src/modules/panel/index.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 
 export const Panel = {
   get: () => {},
-  post: async(app: FastifyInstance) => app.get('/panel', async (request, reply) => {
+  post: async(app: FastifyInstance) => app.post('/panel', async (request, reply) => {
     
   })
 };
