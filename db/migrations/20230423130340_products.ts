9cf6ec0833e16ac9d8865a68dbd6a8f23d7bd8c2
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-06-22 15:36:05 -0300

    [refactor]: new column

--- db/migrations/20230423130340_products.ts
+++ db/migrations/20230423130340_products.ts
@@ -3,27 +3,20 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('products', (table) => {
     table.uuid('id').primary()
+    table.text('user_id').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
     table.text('title').notNullable()
-    table.text('search_code').notNullable()
     table.text('description').notNullable()
+    table.json('material').notNullable()
+    table.decimal('price', 15, 2).notNullable()
     table.json('gender').notNullable()
-    table.text('material').notNullable()
     table.json('color').notNullable()
     table.json('category').notNullable()
-    table.decimal('price', 15, 2).notNullable()
-    table.json('image').notNullable()
+    table.json('card').notNullable()
+    table.text('file_banner').notNullable()
+    table.json('files').notNullable()
     table.json('comments')
     table.json('sales')
     table.timestamp('create_at').defaultTo(knex.fn.now()).notNullable()
-    table.text('session_id').notNullable()
-    table
-      .text('user_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
   })
 }
 
